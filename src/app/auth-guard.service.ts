014aeb4526d019c854eca7834eb662f5ef47bd90
marcinbalut <marcinbalutpoland@gmail.com>
2023-08-02 10:53:08 +0200

    added AuthGuard

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { FakeUserService } from 'src/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private userService: FakeUserService, private router: Router) { }
+
+  canActivate(route, state: RouterStateSnapshot) {
+    if (!this.userService.isLogged) {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+      return false;
+    }
+    return true;    
+  }
+}
