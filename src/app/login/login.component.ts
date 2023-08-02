238b754282b863cfe5a8838aadd307defc267902
marcinbalut <marcinbalutpoland@gmail.com>
2023-08-02 10:55:45 +0200

    added login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FakeUserService } from 'src/services/user.service';
 
 @Component({
   selector: 'login',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  invalidLogin: boolean;
+  isLogged = false;
+  isAdmin = false;
+
+  constructor(
+    private router: Router,
+    private userService: FakeUserService) { }
+
+  login(user) {
+    if (user) {
+      if (this.userService.login(user.username, user.password)) {
+        this.router.navigate(['/']);
+      }
+    }
+
+  }
 }
