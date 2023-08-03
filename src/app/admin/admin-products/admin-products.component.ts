b8f033c27e554d2d052e9c1429e2e194a5d70c1f
marcinbalut <marcinbalutpoland@gmail.com>
2023-08-03 10:40:43 +0200

    updated AdminProduct component

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FakeRepository } from 'src/app/data/fake-repository.service';
 
 @Component({
   selector: 'admin-products',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent {
+  constructor(private repo: FakeRepository) {}
 
+  get products() {
+    return this.repo.getAllProducts();
+  }
 }
