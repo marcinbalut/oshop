30ecb255852f68c1fcc7154d8a4b10a100f24eaf
marcinbalut <marcinbalutpoland@gmail.com>
2023-08-03 10:41:01 +0200

    updated ProductForm component

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { FakeRepository } from 'src/app/data/fake-repository.service';
+import { Category } from 'src/app/data/models/category';
+import { Product } from 'src/app/data/models/product';
 
 @Component({
   selector: 'app-product-form',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent {
+
+  product: Product = { id: '', title: '', price: 0, category: '', imageUrl: '' };//'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png'};
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private repo: FakeRepository) {
+
+
+
+      let id = this.route.snapshot.paramMap.get('id');
+      if(id) {
+        this.product = this.repo.getProduct(id);
+      }
+    }
+
+  get categories(): Category[] {
+    return this.repo.getAllCategories();
+  }
+
+  saveNewProduct(product: Product) {
+    this.repo.createNewProduct(product);
+    this.router.navigate(['/admin/products']);
+  }
 
 }
